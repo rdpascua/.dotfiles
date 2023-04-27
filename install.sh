242d80895a7c22615868d7fcb70d120cb11c1e2a
Ricardo Pascua Jr <rdpascua@outlook.com>
2023-04-27 23:05:16 +0800

    added install script

---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+echo "Installing OMZ..."
+if test ! $(which omz); then
+  /bin/sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/HEAD/tools/install.sh)"
+fi
+
+echo "Installing Homebrew..."
+if test ! $(which brew); then
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+  echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> $HOME/.zprofile
+  eval "$(/opt/homebrew/bin/brew shellenv)"
+fi
+
+brew update
+brew tap homebrew/bundle
+brew bundle --file ./Brewfile
+
+echo "Create Projects"
+mkdir $HOME/Projects
+
